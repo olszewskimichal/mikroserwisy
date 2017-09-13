dc89b53bd00cd54d98ef1ca8e10733e13c62f2df
Michał <michalolsz@mat.umk.pl>
2017-09-13 21:11:12 +0200

    user rest controller - brakuje assercji, testów
    Signed-off-by: Michał <michalolsz@mat.umk.pl>

--- userService/src/main/java/pl/michal/olszewski/userservice/user/UserService.java
+++ userService/src/main/java/pl/michal/olszewski/userservice/user/UserService.java
@@ -16,4 +16,25 @@ public class UserService {
     public Optional<User> getUserByUserName(String userName) {
         return userRepository.findByUserName(userName);
     }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User updateUser(Long id, User user) {
+        User result = null;
+        if (userRepository.exists(id)) {
+            result = this.userRepository.save(user);
+        }
+        return result;
+    }
+
+    public Boolean deleteUser(Long id) {
+        boolean deleted = false;
+        if (userRepository.exists(id)) {
+            this.userRepository.delete(id);
+            deleted = true;
+        }
+        return deleted;
+    }
 }
