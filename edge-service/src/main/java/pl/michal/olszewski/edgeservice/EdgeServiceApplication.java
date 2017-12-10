7a3e6ce2ee470ec7ac5ffab7fc38eba274bb2ceb
Michał <olszewskimichal@outlook.com>
2017-12-10 12:09:19 +0100

    Dodanie edge-service

---
+++ edge-service/src/main/java/pl/michal/olszewski/edgeservice/EdgeServiceApplication.java
@@ -0,0 +1,20 @@
+package pl.michal.olszewski.edgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+@EnableHystrix
+public class EdgeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeServiceApplication.class, args);
+	}
+
+}
